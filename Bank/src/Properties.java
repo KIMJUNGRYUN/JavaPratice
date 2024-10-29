31453726aa9faaba9c7bc5446df9625af0fd44c4
KIM <152789781+KIMJUNGRYUN@users.noreply.github.com>
2024-10-29 16:36:00 +0900

    Delete Bank directory

--- Bank/src/Properties.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-public class Properties {
-    public static void main(String[] args) throws FileNotFoundException {
-        new FileInputStream("/absolute/path/to/config.properties");
-
-    }
-}
